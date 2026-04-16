e4196136a919b0553e9cd5c6903544ae401af002
abhinavaby <abhinavaby07@gmail.com>
2026-04-16 09:42:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function pro1(){
+    window.open("https://abhinavaby.github.io/Pokemon-fetching-API/");
+}
+function pro2(){
+    window.open("https://abhinavaby.github.io/JS-microproject-2/");
+}
+function pro3(){
+    window.open("https://abhinavaby.github.io/JS-microproject-1/");
+}
+function pro4(){
+    window.alert("This project is under development. Please check back later.");
+}
+function pro5(){
+    window.alert("This project is under development. Please check back later.");
+}
+function pro6(){
+    window.alert("This project is under development. Please check back later.");
+}
+function pro7(){
+    window.alert("This project is under development. Please check back later.");
+}
+function pro8(){
+    window.alert("This project is under development. Please check back later.");
+}
